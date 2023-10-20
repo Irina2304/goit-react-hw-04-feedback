5c240f4d2f200d79da36f412e788d167e8555bde
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-10-20 21:42:35 +0200

    after_mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 // import { Component } from 'react';
 import { SectionTitle } from './SectionTitle/SectionTitle';
-import { FeedbackOption } from './FeedbackOptions/FeedbackOptions'
-import { Notification } from "./Notification/Notification";
-import { Statistics } from "./Statistics/Statistics";
-import { useState } from "react";
+import { FeedbackOption } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+import { Statistics } from './Statistics/Statistics';
+import { useState } from 'react';
 
 export const App = () => {
   const [good, setGood] = useState(0);
@@ -14,46 +14,47 @@ export const App = () => {
     good: good,
     neutral: neutral,
     bad: bad,
-  }
+  };
 
-  const onBtnClick = (key) => {
-    key === 'good' ? setGood((prevGood) => prevGood + 1) :
-    key === 'neutral' ? setNeutral((prevNeutral) => prevNeutral + 1) :
-    setBad((prevBad) => prevBad + 1)  
-  }
-  
+  const onBtnClick = key => {
+    switch (key) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      default:
+        setBad(prevBad => prevBad + 1);
+    }
+  };
 
-   const countTotalFeedback = () => {
-     return (good + neutral + bad);
-   }
-  
- 
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
   const countPositiveFeedbackPercentage = () => {
-    const total = countTotalFeedback()
-    return Math.round((good * 100)/total);
-  } 
+    const total = countTotalFeedback();
+    return Math.round((good * 100) / total);
+  };
 
   return (
     <>
-      <SectionTitle title='Please leave feedback'>
-          <FeedbackOption
-          onBtnClick={onBtnClick}
-          value = {Object.keys(value)}
-          />
+      <SectionTitle title="Please leave feedback">
+        <FeedbackOption onBtnClick={onBtnClick} value={Object.keys(value)} />
       </SectionTitle>
 
-      <SectionTitle title='Statistics'>
+      <SectionTitle title="Statistics">
         {countTotalFeedback() ? (
           <Statistics
-            value={ value }
+            value={value}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
         ) : (
-          <Notification title='There is no feedback!'/>
-        )}    
+          <Notification title="There is no feedback!" />
+        )}
       </SectionTitle>
     </>
   );
 };
-
